3f991a3731ddfeee429bc8fe88db9693b6ca6b00
vismodeb <vismodeb2000@gmail.com>
2024-06-27 18:58:24 +0600

    Contact us page created

---
+++ src/components/home/contactUs/ContactUs.jsx
@@ -0,0 +1,17 @@
+const ContactUs = () => {
+  return (
+    <div className="py-11">
+      <h2 className="text-4xl text-center font-semibold">
+        Schedule an Immigration Consultation
+      </h2>
+      <h4 className="text-2xl text-center font-semibold">
+        with Attorney, Nadine Heitz.
+      </h4>
+      <div className="">
+        <h4 className="text-xl font-semibold">Quick Response</h4>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/home/Home/Home";
 import About from "../pages/home/about/About";
+import Booking from "../pages/home/booking/Booking";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: "/about",
         element: <About />,
+      },
+      {
+        path: "/booking",
+        element: <Booking />,
       },
     ],
   },
